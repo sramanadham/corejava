3c47221eb7d5743d852cf6e231b6e1fc3dc53898
Lavanya <Lavanya@192.168.1.10>
2017-08-06 12:56:42 -0500

    psuedo code for tripledouble

--- src/main/java/com/enterprise/core/TripleDouble.java
+++ src/main/java/com/enterprise/core/TripleDouble.java
@@ -3,10 +3,16 @@ package com.enterprise.core;
 public class TripleDouble {
 
 	public int find(int first, int second) {
-		//convert to strings
-		//loop thru first string chars to find 3 consecutive same char
-		//if found 3 consecutive 3 char, check if the same char is repeated two time in second string
-		
+		// convert to strings
+		// loop thru first string chars to find 3 consecutive same char
+			// if found 3 consecutive same char then
+				// if the same char is repeated two times in second string then
+				// return 1
+				//end if
+			//end if
+		//end loop
+		//return 0
+
 		return 0;
 
 	}
